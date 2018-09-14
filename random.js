1a1f995239e0ad11c7d096e0bfbb98fbfb419811
bowenqing <bowenqing@innobuddy.com>
2018-09-14 13:26:18 +0800

    test

--- random.js
+++ random.js
@@ -1,39 +1,38 @@
-const a = {
-  range: [1, 25],
-  get: 0,
-};
-const b = {
-  range: [26, 50],
-  get: 0,
-};
-const c = {
-  range: [51, 100],
-  get: 0,
-};
 
 let rrr;
-const rule = [a, b, c];
-let stack = [];
-function genNum() {
-  let num = Math.ceil(Math.random() * 100);
-  if (rrr) {
-    while (num === undefined || stack.includes(num)) {
-      num = Math.ceil(Math.random() * 100);
-    }
-  }
-  return num;
-}
-
-function inRange(r, num) {
-  const [min, max] = r;
-  if (num >= min && num <= max) {
-    return true;
-  }
-  return false;
-}
-
 
 function test() {
+  const a = {
+    range: [1, 25],
+    get: 0,
+  };
+  const b = {
+    range: [26, 50],
+    get: 0,
+  };
+  const c = {
+    range: [51, 100],
+    get: 0,
+  };
+  const rule = [a, b, c];
+  let stack = [];
+  function genNum() {
+    let num = Math.ceil(Math.random() * 100);
+    if (rrr) {
+      while (num === undefined || stack.includes(num)) {
+        num = Math.ceil(Math.random() * 100);
+      }
+    }
+    return num;
+  }
+  
+  function inRange(r, num) {
+    const [min, max] = r;
+    if (num >= min && num <= max) {
+      return true;
+    }
+    return false;
+  }
   for (let j = 0; j < 100000; j++) {
     const len = Math.ceil(Math.random() * 100);
     stack = [];
@@ -48,9 +47,8 @@ function test() {
     }
   }
   const result = rule.map(x => x.get);
-  
   const sum = result.reduce((s, cur) => (s + cur), 0)
-  console.log(result.map(x => x/sum));
+  console.log(result.map(x => x/sum), rrr);
 }
 
 
