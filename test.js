940ce9dd47c5f634722184265771fa961e23cfab
bowenqing <bowenqing@innobuddy.com>
2018-09-30 11:05:09 +0800

    nth

---
+++ test.js
@@ -0,0 +1,29 @@
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+async function asyncTimeout(func, m) {
+  const timeoutResult = Symbol('timeoutResult')
+  const timeoutP = async () => {
+    await sleep(m)
+    return timeoutResult
+  }
+  const promiseArrasy = [func(), timeoutP(m)]
+  const result = await Promise.race(promiseArrasy)
+  if (result === timeoutResult) {
+    console.log('timeout')
+    throw new Error('timeout')
+  }
+  return result
+}
+
+!(async function name(params) {
+  const a = await asyncTimeout(async () => {
+    await sleep(2000)
+    return 'finish'
+  }, 1000)
+  console.log(a)
+})()
