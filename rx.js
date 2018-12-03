ecb5f13cec675d47bedcf9a6cad2e6a00a215513
bowenqing <bowenqing@innobuddy.com>
2018-12-03 14:09:05 +0800

    test rx

--- rx.js
+++ rx.js
@@ -1,2 +1,11 @@
-const Rx = require('rxjs/Rx')
+var Observable = require('rxjs/Observable').Observable;
+// 使用适合的方法在 Observable 上打补丁
+require('rxjs/add/observable/of');
+require('rxjs/add/operator/map');
+require('rxjs/add/operator/filter');
 
+var s = Observable.of(1,2,3).filter(x => x > 1).map(function (x) { return x + '!!!'; }); // 等等
+
+
+
+s.subscribe(console.log)
